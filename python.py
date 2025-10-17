207fb6d3bc16b1aec07d883afaf25d2f20fd2058
imaduck25 <fatimaa158@nycstudents.net>
2025-10-17 07:14:17 -0700

    hi

--- python.py
+++ python.py
@@ -1,4 +1,17 @@
-count = 1
+""" count = 1
 while count <= 5:
     print("This is loop number", count)
     count = count + 1
+
+order = ""
+while order != "done":
+    order = input("What would you like to order? (type 'done' to finish): ")
+print("Thanks for your order!") """
+
+number_game = ""
+import random
+x = random.randint(1,10)
+while number_game != x:
+    x = input("guess the number i am thinking of. ")
+    break if input == x:
+        print("yayayyayayyay")
